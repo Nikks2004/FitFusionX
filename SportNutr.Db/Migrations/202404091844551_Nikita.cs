09939240d6b26cc79012c1eebff03d528f468202
n3ra6 <n3ra6@example.com>
2024-04-09 23:19:33 +0300

    Create migration

---
+++ SportNutr.Db/Migrations/202404091844551_Nikita.cs
@@ -0,0 +1,77 @@
+﻿namespace SportNutr.Db.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Nikita : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Brands",
+                c => new
+                    {
+                        BrandId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.BrandId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        BrandId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Brands", t => t.BrandId, cascadeDelete: true)
+                .Index(t => t.BrandId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        UserId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Products", t => t.ProductId)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Orders", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "BrandId", "dbo.Brands");
+            DropIndex("dbo.Orders", new[] { "ProductId" });
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropIndex("dbo.Products", new[] { "BrandId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Orders");
+            DropTable("dbo.Products");
+            DropTable("dbo.Brands");
+        }
+    }
+}
